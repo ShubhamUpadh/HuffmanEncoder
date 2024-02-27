76541a1a2d95ca14d398252a9cf08a7d86bde718
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-28 03:25:48 +0530

    Added Error message if file absent, Created Character Frequency Map

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 import sys
 
 class Huffman:
+    
     def __init__(self,fileName):
         self.fileName = fileName + '.txt'
+        self.charFreq = {}      # We are storing this as a dictionary, it can be optimised to an array ????
+    
     def fileCheck(self):
         #print(self.fileName)
         if not os.path.isfile(self.fileName):
@@ -11,6 +14,21 @@ class Huffman:
             sys.exit()
         else:
             print("File exists")
+    
+    def charCount(self):
+        with open(self.fileName,'r',encoding='utf-8') as file:
+            while True:
+                char = file.read(1)
+                if char in self.charFreq:
+                    self.charFreq[char] += 1
+                else:
+                    self.charFreq[char] = 1
+                if not char:
+                    break
+                #print(char)
+        print(self.charFreq['X'],self.charFreq['t'])
+    
 #print(sys.argv[1])
 huffman = Huffman(sys.argv[1])
 huffman.fileCheck()
+huffman.charCount()
--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
 The goals are as 
 1. Understand what is Huffman Encoding
 2. Create a Huffman Encoder
+3. Write unit tests at each and every stage
