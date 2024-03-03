569f994d620042f89dd713221877d4f26e434448
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-03-03 19:29:47 +0530

    Created the code dictionary

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ class Node:
         return self.freq == other.freq
     
 class Huffman:
-    
     def __init__(self,fileName):
         self.fileName = fileName + '.txt'
         self.charFreq = {}      # We are storing this as a dictionary, it can be optimised to an array ????
         self.heap =[]
+        self.root = None        # Store the root of the Binary Tree
+        self.codeDict = {}      # Store the codes here
     
     def fileCheck(self):
         #print(self.fileName)
@@ -49,7 +50,7 @@ class Huffman:
             heapq.heappush(self.heap, btNode)
     
     def buildTree(self):
-        # heap created, now start combining the nodes inside the heap
+        # heap created, now start combining the nodes inside the heap tp form the tree
         while len(self.heap) > 1:
             smallest1 = heapq.heappop(self.heap)
             smallest2 = heapq.heappop(self.heap)
@@ -57,10 +58,22 @@ class Huffman:
             newNode.left = smallest1
             newNode.right = smallest2
             heapq.heappushpush(self.heap, newNode)
-        
-            
-        
-        
+        # This variable will store the root of the Binary Tree
+        self.root = heapq.heappop(self.heap)
+    
+    def createCodeDictHelper(self,node,currPath):
+        # Helper Function for createCodeDict
+        if node == None:
+            return
+        if node.alphabet is not None:
+            self.codeDict[node.alphabet] = currPath
+        self.createCodeDictHelper(self,node.left,currPath+"0")
+        self.createCodeDictHelper(self,node.right,currPath+"1")
+        return    
+    
+    def createCodeDict(self): 
+        # Use the root of the BT to create the dictionary
+        self.createCodeDictHelper(self.root,currPath="")
         
 #print(sys.argv[1])
 huffman = Huffman(sys.argv[1])
